2a8b60b20bb7f43df315276b37a9184d4b972083
rajnish21a <in.rajnish@live.in>
2020-08-11 02:37:58 +0530

    custom-date-picker with Days of week,leap year and no of days consideration

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const date_picker_element = document.querySelector('.date-picker');
 const selected_date_element = document.querySelector('.date-picker .selected-date');
 const dates_element = document.querySelector('.date-picker .dates');
 const mth_element = document.querySelector('.date-picker .dates .month .mth');
+const yr_element = document.querySelector('.date-picker .dates .month .yr');
 const next_mth_element = document.querySelector('.date-picker .dates .month .next-mth');
 const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
 const days_element = document.querySelector('.date-picker .dates .days');
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const days = ['S','M','T','W','T','F','S'];
+
 
 let date = new Date();
 let day = date.getDate();
@@ -18,7 +21,8 @@ let selectedDay = day;
 let selectedMonth = month;
 let selectedYear = year;
 
-mth_element.textContent = months[month] + ' ' + year;
+mth_element.textContent = months[month];
+yr_element.textContent = year;
 
 selected_date_element.textContent = formatDate(date);
 selected_date_element.dataset.value = selectedDate;
@@ -41,9 +45,10 @@ function goToNextMonth (e) {
 	month++;
 	if (month > 11) {
 		month = 0;
-		year++;
+    year++;
+    yr_element.textContent = year;
 	}
-	mth_element.textContent = months[month] + ' ' + year;
+	mth_element.textContent = months[month];
 	populateDates();
 }
 
@@ -51,22 +56,66 @@ function goToPrevMonth (e) {
 	month--;
 	if (month < 0) {
 		month = 11;
-		year--;
+    year--;
+    yr_element.textContent = year;
 	}
-	mth_element.textContent = months[month] + ' ' + year;
+	mth_element.textContent = months[month];
 	populateDates();
 }
 
 function populateDates (e) {
 	days_element.innerHTML = '';
-	let amount_days = 31;
+  let amount_days = 31;
+  
+  switch (month) {
+    case 0:
+    case 2:
+    case 4:
+    case 6:
+    case 7:
+    case 9:
+    case 11:  
+      amount_days = 31;
+      break;
+    case 3:
+    case 5:
+    case 8:
+    case 10:
+      amount_days = 30;
+      break;
+    default:
+      amount_days = 31;
+      break;
+  }
 
-	if (month == 1) {
+	if (month == 1 && (year % 4) !== 0 ) {
 		amount_days = 28;
-	}
+  }
+  
 
+  if (month == 1 && (year % 4) === 0 ) {
+		amount_days = 29;
+  }
+  
+  let day_element;
+  for (let j = 0; j < days.length; j++) {
+    day_element = document.createElement('div');
+		day_element.classList.add('day');
+    day_element.textContent = days[j];
+    days_element.appendChild(day_element);
+  }
+
+  let firstDay = new Date(year,month, 1);
+	let lastDay = new Date(year, month+1, 0);
+  let offset = firstDay.getDay();
+  for (let k = 0; k < offset; k++) {
+    day_element = document.createElement('div');
+		day_element.classList.add('day');
+    days_element.appendChild(day_element);
+  }
+  
 	for (let i = 0; i < amount_days; i++) {
-		const day_element = document.createElement('div');
+		day_element = document.createElement('div');
 		day_element.classList.add('day');
 		day_element.textContent = i + 1;
 
@@ -113,5 +162,5 @@ function formatDate (d) {
 
 	let year = d.getFullYear();
 
-	return day + ' / ' + month + ' / ' + year;
+	return day + '/' + month + '/' + year;
 }
